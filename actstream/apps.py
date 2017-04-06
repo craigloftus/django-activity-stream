c3cba5b9912c6e7f90e6d98acbf8c80bb8a985b7
Craig Loftus <craigloftus+github@gmail.com>
2017-04-06 10:39:08 +0100

    Removing jsonfield_compat
    The post_migrate signal used by jsonfield_compat clashes with our use of django-tenant-schemas

--- actstream/apps.py
+++ actstream/apps.py
@@ -14,13 +14,5 @@ class ActstreamConfig(AppConfig):
         action_class = self.get_model('action')
 
         if settings.USE_JSONFIELD:
-            try:
-                from jsonfield_compat import JSONField, register_app
-            except ImportError:
-                raise ImproperlyConfigured(
-                    'You must have django-jsonfield-compat installed '
-                    'if you wish to use a JSONField on your actions'
-                )
+            from django.contrib.postgres.fields import JSONField
             JSONField(blank=True, null=True).contribute_to_class(action_class, 'data')
-
-            register_app(self)
